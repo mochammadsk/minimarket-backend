9887ef551496d48698f491f5962458a57889eec6
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2025-01-15 15:51:40 +0800

    add Mongoose integration and import ProductsModule in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/minimarket'),
+    ProductsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
