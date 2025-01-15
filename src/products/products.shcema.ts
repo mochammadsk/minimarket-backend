023a3f56fb3616ce259cbf847dda2807fd37adb5
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2025-01-15 15:51:32 +0800

    add Product schema for MongoDB with validation properties

---
+++ src/products/products.shcema.ts
@@ -0,0 +1,20 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema({
+  timestamps: true,
+  versionKey: false,
+})
+export class Product extends Document {
+  @Prop({ required: true })
+  name: string;
+  description: string;
+
+  @Prop({ min: 0 })
+  price: number;
+
+  @Prop({ min: 1 })
+  rating: number;
+}
+
+export const ProductSchema = SchemaFactory.createForClass(Product);
