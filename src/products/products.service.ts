5f54eeb5b40000bdc0a7028177dad3fc25ef5ec9
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2025-01-18 03:17:14 +0800

    enhance ProductsService with conflict and not found exceptions in create and findAll methods

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { InjectModel } from '@nestjs/mongoose';
@@ -11,13 +15,28 @@ export class ProductsService {
     @InjectModel(Product.name) private productModel: Model<Product>,
   ) {}
 
-  create(createProductDto: CreateProductDto): Promise<Product> {
+  async create(createProductDto: CreateProductDto): Promise<Product> {
+    const products = await this.productModel
+      .findOne({ name: createProductDto.name })
+      .exec();
+
+    if (products) {
+      throw new ConflictException('Product already exists');
+    }
+
     const createdProduct = new this.productModel(createProductDto);
+
     return createdProduct.save();
   }
 
-  findAll() {
-    return `This action returns all products`;
+  async findAll(): Promise<Product[]> {
+    const products = await this.productModel.find().exec();
+
+    if (!products || products.length === 0) {
+      throw new NotFoundException('Products not found');
+    }
+
+    return products;
   }
 
   findOne(id: number) {
