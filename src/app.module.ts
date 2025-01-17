e6343a1ea2884644cce45967afd3dd9ae453e66e
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2025-01-18 03:22:42 +0800

    add global configuration module for environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -7,6 +8,9 @@ import { ProductsModule } from './products/products.module';
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/minimarket'),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     ProductsModule,
   ],
   controllers: [AppController],
