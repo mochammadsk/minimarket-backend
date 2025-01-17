ba32edc4462ec05a3990b0af52c6c2f85039dfdd
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2025-01-18 02:52:44 +0800

    add CORS configuration in main.ts for local development

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // CORS
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept',
+    credentials: false,
+  });
+
+  await app.listen(process.env.PORT);
 }
 bootstrap();
