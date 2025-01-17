1717afd76e38910d1fd4e8ec26f2d7e22a915ef0
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2025-01-18 02:52:54 +0800

    add validation rules to CreateProductDto for name, description, price, and stock

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,26 @@
-export class CreateProductDto {}
+import {
+  IsString,
+  IsNumber,
+  IsOptional,
+  Min,
+  MaxLength,
+} from 'class-validator';
+
+export class CreateProductDto {
+  @IsString()
+  @MaxLength(50)
+  name: string;
+
+  @IsString()
+  @IsOptional()
+  @MaxLength(200)
+  description?: string;
+
+  @IsNumber()
+  @Min(0)
+  price: number;
+
+  @IsNumber()
+  @Min(0)
+  stock: number;
+}
